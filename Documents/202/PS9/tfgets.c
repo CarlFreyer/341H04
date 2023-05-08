1471301477c0954b69fdc32edaf66834b8fb8c7c
ckf225 <ckf225@lehigh.edu>
2023-05-08 15:28:49 -0400

    new

---
+++ Documents/202/PS9/tfgets.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+                                      
+typedef void handler_t;
+
+void tfgets_alrm(int sig){
+    return NULL;
+}
+
+int main(){
+  char buffer[500];
+  tfgets(buffer, 500, stdin);
+  printf("%s", buffer);
+}
+
+void tfgets(char *s, int size, FILE *stream){
+  Signal(SIGALRM, tfgets_alrm);
+  alarm(5);
+  fgets(s, size, stream);
+}
+
+int Sigemptyset(sigset_t *set){//sigemptyset
+  int ret;
+  if ((ret = sigemptyset(set)) < 0){
+    unix_error("Signal Error");
+  }
+  return ret;
+}
+
+handler_t *Signal(int signum, handler_t *handler){//signal
+  struct sigaction action, old_action;
+  action.sa_handler = handler;
+  Sigemptyset(&action.sa_mask);
+  action.sa_flags = SA_RESTART;
+  if (Sigaction(signum, &action, &old_action) == SIG_ERR)
+  {
+    unix_error("Handler error");
+  }
+  return (old_action.sa_handler);
+}
